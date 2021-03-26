d2632263965599bd2fa39be7441d266fd6b57116
lius158 <lius158@chinaunicom.cn>
2021-03-26 09:32:50 +0800

    更新控制条

--- app/src/main/java/com/solo/customview/MainActivity.java
+++ app/src/main/java/com/solo/customview/MainActivity.java
@@ -1,22 +1,51 @@
 package com.solo.customview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
 
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.solo.customview.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatSeekBar sbBar;
+    private View3D view3D;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView v = findViewById(R.id.tv);
+/*        TextView v = findViewById(R.id.tv);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.scaleanim);
-        v.startAnimation(animation);
+        v.startAnimation(animation);*/
+
+
+        initView();
+    }
+
+    private void initView() {
+        sbBar = (AppCompatSeekBar) findViewById(R.id.sb_bar);
+        sbBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                view3D.setProgress(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        view3D = (View3D) findViewById(R.id.view3D);
     }
 }
--- app/src/main/java/com/solo/customview/View3D.java
+++ app/src/main/java/com/solo/customview/View3D.java
@@ -1,5 +1,6 @@
 package com.solo.customview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,18 +9,22 @@ import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
 import com.solo.customview.R;
 
-public class View3D extends androidx.appcompat.widget.AppCompatImageView {
+@SuppressLint("AppCompatCustomView")
+public class View3D extends ImageView {
 
     private Paint mPaint;
     private Bitmap mBitmap;
     private Camera camera = new Camera();
     private Matrix matrix = new Matrix();
     private int mProgress = 80;
+
+    private static final String TAG = "View3D";
 
     public View3D(Context context) {
         super(context);
@@ -43,27 +48,24 @@ public class View3D extends androidx.appcompat.widget.AppCompatImageView {
         mPaint.setAntiAlias(true);
     }
 
-    private void setProgress(int progress){
+    public void setProgress(int progress){
         mProgress = progress;
         postInvalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
+//        canvas.drawBitmap(mBitmap, new Rect(0,0,1920,1080),new Rect(0,0,getWidth(),getHeight()),mPaint);
+//        Log.d(TAG, "onDraw: mProgress" + mProgress);
         camera.save();
         canvas.save();
 
-
         mPaint.setAlpha(100);
         canvas.drawBitmap(mBitmap, 0, 0, mPaint);
-//        canvas.drawBitmap(mBitmap, new Rect(0,0,1920,1080),new Rect(0,0,getWidth(),getHeight()),mPaint);
         camera.rotateY(mProgress);
-//        camera.translate(1200, 0, 0);
         camera.applyToCanvas(canvas);
-        super.onDraw(canvas);
-
         camera.restore();
+        super.onDraw(canvas);
         canvas.restore();
 
     }
